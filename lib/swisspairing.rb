f5006119ecb8401ea101884ee48c0d9621bf48b7
Lapo Elisacci <lapoelisacci@gmail.com>
2025-05-02 20:07:26 +0200

    First commit

---
+++ lib/swisspairing.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "swisspairing/version"
+
+module Swisspairing
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/swisspairing_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Swisspairing do
+  it "has a version number" do
+    expect(Swisspairing::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
